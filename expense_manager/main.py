68d1e127989a63da1b1aaa4dee475b59387cd57e
Praneet M <praneetm@Praneets-MacBook-Air.local>
2026-03-09 23:53:36 +0530

    3 : added menu system and view expense system

--- expense_manager/main.py
+++ expense_manager/main.py
@@ -3,16 +3,33 @@ from storage import load_expenses, save_expenses
 
 expenses = load_expenses()
 
-print(expenses)
+print("1. Add Expense")
+print("2. View Expenses")
+print("3. Exit")
 
-amount = input("Enter the amount: ")
-category = input("Enter the category: ")
-description = input("Enter the description: ")
-date = input("Enter the date (DD-MM-YYYY: ")
+choice = input("Choose an option: ")
+ 
+if choice == "1":
+   
+   amount = input("Enter the amount: ")
+   category = input("Enter the category: ")
+   description = input("Enter the description: ")
+   date = input("Enter the date (DD-MM-YYYY: ")
 
-new_expense = Expense(amount, category, description, date)
-expense_dict = new_expense.to_dict()
-expenses.append(expense_dict)
+   new_expense = Expense(amount, category, description, date)
+   expense_dict = new_expense.to_dict()
+   expenses.append(expense_dict)
 
-save_expenses(expenses)
+   save_expenses(expenses)
+
+elif choice == "2":
+   for expense in expenses:
+       print("Amount:", expense["amount"])
+       print("Category:", expense["category"])
+       print("Description:", expense["description"])
+       print("Date:", expense["date"])
+       print("--------------------")
+
+elif choice == "3":
+    print("Exiting the program.")   
 
