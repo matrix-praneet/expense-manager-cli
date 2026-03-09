2dc34863d0ddc5ebadbe7681e5c2f142a52f93aa
Praneet M <praneetm@Praneets-MacBook-Air.local>
2026-03-09 22:03:15 +0530

    2: implemented add expense and JSON saving

--- expense_manager/main.py
+++ expense_manager/main.py
@@ -1,5 +1,18 @@
-from storage import load_expenses
+from expense import Expense
+from storage import load_expenses, save_expenses
 
 expenses = load_expenses()
 
 print(expenses)
+
+amount = input("Enter the amount: ")
+category = input("Enter the category: ")
+description = input("Enter the description: ")
+date = input("Enter the date (DD-MM-YYYY: ")
+
+new_expense = Expense(amount, category, description, date)
+expense_dict = new_expense.to_dict()
+expenses.append(expense_dict)
+
+save_expenses(expenses)
+
--- expense_manager/storage.py
+++ expense_manager/storage.py
@@ -4,3 +4,7 @@ def load_expenses():
     with open("data.json", "r") as file:  # with automatically closes the file after reading.
         data = json.load(file)    # json.load() reads the JSON data from the file and converts it into a Python list
         return data
+    
+def save_expenses(expenses):
+    with open("data.json", "w") as file:  # with automatically closes the file after writing.
+        json.dump(expenses, file)   # json.dump() takes a Python and writes it to the file in JSON format.
